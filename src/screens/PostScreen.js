008ddb7a3ca8c9fd09d3c5e4ce632bf0ea1fbff2
Iuliia <iuliiadmytryna@gmail.com>
2020-08-03 16:03:13 +0300

    posts screen styles

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, Text, StyleSheet, Image, Button, ScrollView, Alert} from 'react-native';
+import { THEME } from '../theme';
+import { DATA } from '../data';
 
 export const PostScreen = ({navigation}) => {
     const postId = navigation.getParam('postId')
-return(
-    <View style = {styles.center}>
-        <Text>{postId}</Text>
-    </View>
+    const post = DATA.find(p => p.id === postId)             //константа post для того, щоб дістати img нижче відповідно до вибраного id
+    const removeHandler = () => {
+        Alert.alert(
+            'Delete Post',
+            'Are you sure?',
+            [
+              {
+                text: 'Cancel',
+                style: 'cancel'
+              },
+              { text: 'OK', onPress: () => {}}
+            ]
+          );
+    }
+    return(
+    <ScrollView>
+        <Image source = {{uri: post.img}} style = {styles.image}/>
+        <View style = {styles.textWrap}>
+    <Text style = {styles.text}>{post.text}</Text>
+        </View>
+        <Button title = 'Delete' color = {THEME.DANGER_COLOR} onPress = {removeHandler}/>
+    </ScrollView>
 )
 }
 
 PostScreen.navigationOptions = ({navigation}) => {
-    //const postId = navigation.getParam('postId')
     const date = navigation.getParam('date')
     return {
         headerTitle: 'Post from ' + new Date(date).toLocaleDateString()
@@ -19,9 +38,14 @@ PostScreen.navigationOptions = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    text: {
+        fontFamily: 'open-require'
+    },
+    textWrap: {
+        padding: 10
     }
 })
