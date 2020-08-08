477310df5edbb82d8c5871a9b3944294fee4a46a
Iuliia <iuliiadmytryna@gmail.com>
2020-08-08 17:00:03 +0300

    deployment set up

---
+++ src/store/reducers/post.js
@@ -0,0 +1,11 @@
+const initialState = {
+    allPosts: [],                //початковий стан - пустий масив
+    bookedPosts: []
+}
+
+export const postReducer = (state = initialState, action) => {
+return state
+}
+
+
+
